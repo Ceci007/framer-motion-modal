8b1a897de60ab206adf3b85860491f961594bedb
UnicornDad8 <cecibenitezca@gmail.com>
2025-10-07 10:38:55 -0300

    Started button motion and folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { motion } from "framer-motion";
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="save-button"
+        onClick={() => null}
+      >
+        Launch Modal
+      </motion.button>
     </div>
   );
 }
